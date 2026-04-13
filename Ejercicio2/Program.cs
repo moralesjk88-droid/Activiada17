559a8e245680794d698e35c8e61ae94503d87336
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-13 07:20:03 -0600

    Ejercicio2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double precio;
+bool calcular;
+do
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese el precio");
+    calcular=double.TryParse(Console.ReadLine(), out precio);
+    if (!calcular)
+    {
+        Console.WriteLine("ERROR DE PRECIO");
+        Console.ReadKey();
+    }
+    
+    else
+    {
+        if (precio<=0)
+        {
+            Console.WriteLine("Precio no valido");
+            Console.ReadKey();
+        }
+        else
+        {
+          Console.WriteLine("Precio guardado"); 
+            Console.ReadKey();
+        }
+        
+    }
+
+
+}
+while (!calcular||precio<0);

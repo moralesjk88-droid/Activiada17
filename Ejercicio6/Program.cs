15cf2ef9971ed65e183be2d197e54bc9ea137892
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-13 07:59:54 -0600

    ejercicio6

---
+++ Ejercicio6/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+DateTime fecha;
+bool esFecha;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese una fecha");
+    esFecha=DateTime.TryParse(Console.ReadLine(), out fecha);
+
+    if (!esFecha)
+    {
+        Console.WriteLine("Fecha invalida");   
+        Console.ReadKey();
+    }
+
+
+}
+while (!esFecha);
+
+Console.WriteLine("Fecha corta: " + fecha.ToShortDateString());
+Console.ReadKey();

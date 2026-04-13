6ab3bd55a4e056599d6abd09d0c9480e2ab369d7
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-13 08:26:56 -0600

    Ejercicio9

---
+++ Ejercicio9/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+double nota;
+bool esNumero;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese nota:");
+    esNumero = double.TryParse(Console.ReadLine(), out nota);
+
+    if (!esNumero)
+    {
+        Console.WriteLine("Debe ingresar número");
+        Console.ReadKey();
+    }
+    else if (nota < 0 || nota > 100)
+    {
+        Console.WriteLine("Fuera de rango");
+        Console.ReadKey();
+
+    }
+
+} while (!esNumero || nota < 0 || nota > 100);
+
+if (nota >= 60)
+{
+    Console.WriteLine("Aprobado");
+
+}
+else
+{
+    Console.WriteLine("Reprobado");
+}

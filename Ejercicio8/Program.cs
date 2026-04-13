395c563645aa8c5348dfc2081c98133154894905
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-13 08:18:37 -0600

    ejercicio8

---
+++ Ejercicio8/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+double num;
+bool esNumero;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese número:");
+    esNumero = double.TryParse(Console.ReadLine(), out num);
+
+    if (!esNumero)
+    {
+        Console.WriteLine("Debe ingresar número");
+        Console.ReadKey();
+    }
+    else if (num < 0)
+    {
+        Console.WriteLine("No puede ser negativo");
+Console.ReadKey();
+    }
+
+} while (!esNumero || num < 0);
+
+Console.WriteLine("Raíz: " + Math.Sqrt(num));

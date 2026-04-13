8681a0bd68f132d0ff0aed5610dfb3fe3431a0b1
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-13 07:52:39 -0600

    Ejercicio5

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+bool esNumero;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese un numero");
+    esNumero = int.TryParse(Console.ReadLine(), out num);
+    if (!esNumero)
+    {
+        Console.WriteLine("Debe ingresar un numero");
+    }
+
+}
+while (!esNumero);
+
+if (num%2==0)
+{
+    Console.WriteLine("Es par");
+    }
+else
+{
+    Console.WriteLine("Es impar");
+}

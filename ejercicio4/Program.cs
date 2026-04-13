f734a8d66c286e3f32fb9972ae1ead0ef2acc3a8
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-13 07:41:16 -0600

    Ejercicio4

---
+++ ejercicio4/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+int año;
+bool esNumero;
+int actual=DateTime.Now.Year;
+
+do
+{
+    Console.WriteLine("Ingrese año de nacimiento");
+    esNumero=int.TryParse(Console.ReadLine(), out año);
+
+    if (!esNumero)
+    {
+        Console.WriteLine("Debe ingresar un numero");
+    Console.ReadKey();
+    }
+    else if (año<1900||año>actual)
+    {
+        Console.WriteLine("Año fuera de rengo");
+   Console.ReadKey();
+    }
+    else
+    {
+        Console.WriteLine("Año valido");
+        Console.ReadKey();
+    }
+
+}
+while (!esNumero||año<1900||año>actual);
+
+

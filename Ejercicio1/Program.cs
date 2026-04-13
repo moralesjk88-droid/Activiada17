54d4ba728e9909ec504d04da386c00e9899d8b71
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-13 07:07:38 -0600

    ejercicio1

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int edad;
+do
+{
+ Console.Clear();
+Console.WriteLine("ingrese su edad");
+
+    if (int.TryParse(Console.ReadLine(), out edad))
+    {
+        if (edad < 0|| edad>100)
+        { 
+            Console.WriteLine("Edad no valida");
+          Console.ReadKey();
+        }
+        else
+        {
+             Console.WriteLine("Edad aguardada");
+        Console.ReadKey();
+        }
+    }
+    else
+    {
+        Console.WriteLine("EDAD NO PERMITIDA");
+    }
+
+} while (edad<0||edad>100);
+
+
+
+

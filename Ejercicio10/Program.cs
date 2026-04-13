d8505e6da4d8a7767839cd42d51d64f66d655d86
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-13 08:36:27 -0600

    Ejercicio10

---
+++ Ejercicio10/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+int cantidad;
+bool esNumero;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Cantidad de productos:");
+    esNumero = int.TryParse(Console.ReadLine(), out cantidad);
+
+    if (!esNumero)
+    {
+        Console.WriteLine("Debe ingresar número");
+        Console.ReadKey();
+
+    }
+    else if (cantidad <= 0)
+    {
+        Console.WriteLine("Debe ser positivo");
+        Console.ReadKey();
+    }
+
+} while (!esNumero || cantidad <= 0);
+
+for (int i = 1; i <= cantidad; i++)
+{
+    Console.WriteLine("Producto " + i);
+}
+
+

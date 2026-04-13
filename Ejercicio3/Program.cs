83e47cc2a0720dc5334aa9fc4e7bfff7a782c799
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-13 07:31:20 -0600

    Ejercicio3

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+int estudiante;
+bool esNumero;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese la cantidad de estudiantes");
+    esNumero=int.TryParse(Console.ReadLine(), out estudiante);
+    if (!esNumero)
+    {
+        Console.WriteLine("ERROR: NUMERO NO VALIDO");
+        Console.ReadKey();
+    }
+    else if (estudiante<=0)
+    {
+        Console.WriteLine("Debe ser posiva");
+    }
+    else
+    {
+        Console.WriteLine("Cantidad guardada");
+    }
+
+
+
+}
+while (!esNumero||estudiante<=0);
+
+
+
+
+

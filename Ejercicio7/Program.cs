2205e6ab8075ce0b9f5479d7de5b51adaa2f1df8
moralesjk88-droid <moralesjk88@gmail.com>
2026-04-13 08:11:53 -0600

    Ejercicio7

---
+++ Ejercicio7/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+double salario;
+bool esNumero;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese salario:");
+    esNumero = double.TryParse(Console.ReadLine(), out salario);
+
+    if (!esNumero)
+    {
+Console.WriteLine("Debe ingresar número");
+        Console.ReadKey();
+    }
+        
+    else if (salario < 0)
+    {
+Console.WriteLine("No puede ser negativo");
+        Console.ReadKey();
+    }
+        
+    else
+    {
+ Console.WriteLine("Salario válido");
+        Console.ReadKey();
+    }
+       
+
+} while (!esNumero || salario < 0);
